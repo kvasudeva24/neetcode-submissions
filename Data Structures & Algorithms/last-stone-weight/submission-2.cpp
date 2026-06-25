1cc26b7be2cf939834c85ff77d2e5dd116077f19
Kartik Vasudeva <152089076+kvasudeva24@users.noreply.github.com>
2026-06-24 19:47:04 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+
+        for ( int num : stones ) {
+            pq.push(num);
+        }
+
+        while ( pq.size() > 1 ) {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+
+            if ( x == y ) {
+                continue;
+            }
+
+            pq.push(abs(x-y));
+
+        }
+
+        return pq.empty() ? 0 : pq.top();
+    }
+};
